003f79608c9ff404cc82522c249e6755474e73bd
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2022-02-10 04:58:16 -0800

    KM curve with lifelines

---
+++ src/2022-02-10_km-curve-lifelines.py
@@ -0,0 +1,24 @@
+"""
+
+
+"""
+
+from lifelines import KaplanMeierFitter
+from lifelines.datasets import load_waltons
+import matplotlib.pyplot as plt
+
+from IPython.core.interactiveshell import InteractiveShell  # noqa
+
+InteractiveShell.ast_node_interactivity = "all"
+
+# ## Example 1
+waltons = load_waltons()
+waltons.columns = ["time", "event", "group"]
+waltons.describe(include="all").T
+
+kmf = KaplanMeierFitter(label="waltons_data")
+kmf.fit(waltons["time"], waltons["event"])
+kmf.plot_survival_function()
+plt.show()
+
+# ## Example 2
