fc0a75787f0cb803aea986d45acbf1b6495515c6
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2024-02-08 13:15:02 -0800

    Add notes on heuristic calculation of E(num events) from KM curve

--- src/2023-12-08_km-curve-for-predicting-number-of-events.py
+++ src/2023-12-08_km-curve-for-predicting-number-of-events.py
@@ -21,7 +21,9 @@ what is E(num_event) for an entity that has just entered the analysis. It does n
 account for cases where the new entity has already survived for X hours.
 
 We need a way to generate conditional event tables (alongside conditional KM curves,
-which we already do) in order to answer this question.
+which we already do) in order to answer this question. Alternatively, maybe we can
+just take the difference between two unconditional estimates? See
+`n_events_between_10_and_20` below.
 """
 
 import lifelines
@@ -33,6 +35,36 @@ import pandas as pd
 def predict_events_for_new_person_using_event_table(
     prediction_time: float, event_table: pd.DataFrame, df_tbe: pd.DataFrame
 ) -> float:
+    """
+    This is a heuristic method for estimating the expected number of events that a
+    new unit (starting at time 0) will experience by time=`prediction_time`.
+
+    The logic is similar to how the MCF works: We average the total number of events
+    observed up to time point `prediction_time` across the number of units that
+    are at risk at the start.
+
+    There is a critical difference between the way we define the number of units at
+    risk here vs how it shows up in the KM event table. In the event table, each
+    TBE interval is assumed to be associated with a separate unit, because it does
+    not account for the recurrent nature of the data (which actually allows one unit to
+    contribute multiple TBE intervals).
+
+    Example:
+    - prediction_time = 5.0
+    - number of observed events = 6
+    - number of contributing units = 8
+    - expected number of events per unit within 5 time units = 6/8 = .75
+
+    Breaking this down to the level of each id:
+    - id=1 had 1 event on or before t=5
+    - id=2 had 0 events on or before t=5
+    - id=3 had 0 events on or before t=5
+    - id=4 had 1 event on or before t=5
+    - id=5 had 1 event on or before t=5
+    - id=6 had 1 event on or before t=5
+    - id=7 had 1 event on or before t=5
+    - id=8 had 1 event on or before t=5
+    """
     num_unique_units = df_tbe["id"].nunique()
 
     prediction = (
@@ -45,17 +77,25 @@ def predict_events_for_new_person_using_event_table(
 def predict_prob_one_or_more_events_by_specified_time(
     kmf: lifelines.fitters.kaplan_meier_fitter.KaplanMeierFitter, prediction_time: int
 ) -> float:
+    """
+    This is the current methodology used in the MHLH project to get predicted
+    survival probabilities. Since we are in the recurrent events context, it is more
+    correct to interpret these results as "prob of one *or more* events in the given
+    timeframe", rather than "prob of an event in the given timeframe".
+    """
     return kmf.predict(prediction_time)
 
 
 def generate_conditional_event_table():
-    # see overview above
+    # See overview above. This may not be necessary if we just take differences across
+    # unconditional estimates.
     pass
 
 
 # Main analysis:
 df_tbe = pd.DataFrame(
     {
+        "id": [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
         "value": [10, 5, 10, 20, 1, 10, 3, 8, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1],
         "is_uncensored": [1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
     }
@@ -67,25 +107,56 @@ kmf.plot_survival_function()
 plt.show()
 kmf.event_table
 
+"""
+Note that if we simply count the "dips" in the KM curve, we will grossly overestimate
+the number of events a new id will have in time<=10. This is because we are ignoring
+the recurrent nature of the data.
+"""
+
 
 predicted_events = predict_events_for_new_person_using_event_table(
     10, kmf.event_table, df_tbe
 )
 
 thresh = 1e-2
-expected_result = 1.25
+expected_result = 1.25  # makes sense given the number of events each person has had
 assert abs(predicted_events - expected_result) <= thresh
 
 predict_prob_one_or_more_events_by_specified_time(kmf, 10)
 
 
 # Some more examples
+# Note that a longer timeframe should always have the same number or more expected
+# events than a shorter timeframe.
 pred_times = [2, 5, 10, 12, 15, 20, 50, 100]
 for time in pred_times:
     pred = predict_events_for_new_person_using_event_table(
         time, kmf.event_table, df_tbe
     )
     print(f"time: {time}, pred={pred}")
+
+
+# We can find E(num_events) within a specific timeframe by taking the difference
+# between two unconditional estimates:
+n_events_at_10 = predict_events_for_new_person_using_event_table(
+    10, kmf.event_table, df_tbe
+)
+n_events_at_20 = predict_events_for_new_person_using_event_table(
+    20, kmf.event_table, df_tbe
+)
+n_events_between_10_and_20 = n_events_at_20 - n_events_at_10
+assert n_events_between_10_and_20 == 0.125
+
+
+# Cumulative hazard function using Nelson-Aalen:
+naf = lifelines.NelsonAalenFitter().fit(df_tbe["value"], df_tbe["is_uncensored"])
+naf.plot()
+plt.show()
+
+"""
+Similar to the naive application of the KM curve for recurrent data, this will give an
+incorrect estimate of number events.
+"""
 
 
 # Trying with CoxPH model to show that it's equivalent to KM when no covariates
@@ -97,10 +168,13 @@ cph = lifelines.CoxPHFitter().fit(
 )
 cph.print_summary()
 prediction_time = 10
-predicted_cumulative_hazard = cph.predict_cumulative_hazard(
-    pd.DataFrame(), times=prediction_time
-)
-print(predicted_cumulative_hazard.values[0][0])
+try:
+    predicted_cumulative_hazard = cph.predict_cumulative_hazard(
+        pd.DataFrame(), times=prediction_time
+    )
+    print(predicted_cumulative_hazard.values[0][0])
+except KeyError:
+    "doesn't work"
 
 
 # todo: try coxph with 1 predictor - `tbe_sequence_number`. This let's us analyze
