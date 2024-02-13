5ea89a9cd3148259169bc0548c5f36657d7e5a2d
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2024-02-12 16:41:44 -0800

    Add function for generating data from single unit

--- src/2023-12-08_km-curve-for-predicting-number-of-events.py
+++ src/2023-12-08_km-curve-for-predicting-number-of-events.py
@@ -29,7 +29,11 @@ just take the difference between two unconditional estimates? See
 import lifelines
 from lifelines import KaplanMeierFitter
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import scipy.stats
+from scipy.stats import exponweib, lognorm
+from typing import Tuple
 
 
 def predict_events_for_new_person_using_event_table(
@@ -90,6 +94,60 @@ def generate_conditional_event_table():
     # See overview above. This may not be necessary if we just take differences across
     # unconditional estimates.
     pass
+
+
+def generate_data_single_unit(
+    distribution: scipy.stats._continuous_distns,
+    horizon: int,
+    params: dict,
+    seed: int = None,
+) -> Tuple[np.array, np.array]:
+    """
+    Generates a set of TBE values from the given distribution, as well as the
+    is_uncensored indicator values.
+    """
+    try:
+        dist = distribution(**params)
+    except TypeError as e:
+        print(f"ERROR: {e}")
+        raise TypeError("SUGGESTION: Check that parameters match given distribution")
+
+    samples_sum = 0
+    n = int(np.ceil(horizon / 50))  # heuristic
+    while samples_sum < horizon:
+        samples = dist.rvs(n, random_state=seed)
+        samples_sum = samples.sum()
+        n *= 2
+
+    sample_values, is_uncensored = assign_censoring(samples, horizon)
+    assert sample_values.shape[0] == is_uncensored.shape[0]
+    return sample_values, is_uncensored
+
+
+def assign_censoring(
+    samples: np.array, horizon: int, tolerance: float = 1e-3
+) -> Tuple[np.array, np.array]:
+    """
+    Checks for the point at which the sampled TBE intervals add up to a value greater
+    than the simulation horizon. When this happens, we cut off and censor the last TBE
+    interval.
+    """
+    samples_cumsum = np.cumsum(samples)
+    check_greater_than_horizon = samples_cumsum > horizon
+    censored_sample_index = check_greater_than_horizon.tolist().index(True)
+
+    sample_values = samples[:censored_sample_index]
+    last_sample = horizon - samples_cumsum[censored_sample_index - 1]
+    sample_values = np.append(sample_values, [last_sample])
+    assert np.abs(sample_values.sum() - horizon) < tolerance
+
+    is_uncensored = np.append(np.ones(censored_sample_index), np.zeros(1))
+    return sample_values, is_uncensored
+
+
+SEED = 2024
+generate_data_single_unit(exponweib, 999, {"a": 1, "c": 0.9, "scale": 100}, seed=SEED)
+generate_data_single_unit(lognorm, 999, {"s": 0.8, "scale": 100}, seed=SEED)
 
 
 # Main analysis:
